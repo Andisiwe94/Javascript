a32c8eb20ef2e00b2aed77f89f90010350b0c379
unknown <andisiwegungu04@gmail.com>
2021-11-08 15:57:18 +0200

    let arrays added

---
+++ script.js
@@ -0,0 +1,11 @@
+let array =[];
+
+let array = ["Sophie","mmasepaka","Wandile","kagisho","happy","andisiwe","sithemba","selina","lucky","Khum", "vusi", "hardy", ""];
+  
+let display = document.getElementById("display");
+
+for(let i =0; i <array.length; i ++) {
+    display.innerHTML, 
+
+};
+
